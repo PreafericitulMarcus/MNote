566c27fe798045087b4646d496884e58dd8f0fea
Preafericitul_Marcus <marcus888coman@gmail.com>
2025-07-09 01:42:21 +0300

    tyring stuff out

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+
+#include <QApplication> 
+#
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    
+    QApplication::setApplicationName("MNote");
+    // ? TODO: Add font maybe ? addApplicationFont <- consolas ?
+
+    MainWindow window;
+    window.show();
+    
+    return app.exec();
+}
